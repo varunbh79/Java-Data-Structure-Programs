314ef2e75032ab11afc0d83f49691d69926ea9ee
Varun <varunbhardwaj79@gmail.com>
2019-09-18 12:48:36 +0530

    Minor Changes
    Removal of comments and unwanted spaces

--- src/com/datastructures/programs/ArrayListRemoveDemo.java
+++ src/com/datastructures/programs/ArrayListRemoveDemo.java
@@ -12,20 +12,13 @@ public class ArrayListRemoveDemo {
 
         String[] languageNames = {"C", "C++", "Java", "Scala", "Scala", "JavaScript", "Python", "Ruby", "Perl", "Groovy", "Shell Script"};
         List<String> programmingLanguages = new ArrayList ( Arrays.asList ( languageNames ) );
-        //programmingLanguages.remove(4);
         System.out.println ( "Removing by index " + programmingLanguages );
         boolean isRemoved = programmingLanguages.remove ( "Shell Script" );
         System.out.println ( "Removing by object : " + isRemoved + programmingLanguages );
-
-        List<String> scriptingLanguages = new ArrayList<> ( Arrays.asList (
-                "Groovy", "Perl", "Python", "Ruby"
-                                                                          ) );
-
+        List<String> scriptingLanguages = new ArrayList<> ( Arrays.asList ( "Groovy", "Perl", "Python", "Ruby" ) );
         List<String> functionalLanguages = new ArrayList<> ( Arrays.asList ( "Scala", "JavaScript", "Java", "Python" ) );
-
         boolean isScriptingLangRemoved = programmingLanguages.removeAll ( scriptingLanguages );
         System.out.println ( "After Removing Scripting" + isScriptingLangRemoved + programmingLanguages );
-
         programmingLanguages.removeIf ( language -> !functionalLanguages.contains ( language ) );
         System.out.println ( "Functional Languages : " + programmingLanguages );
         System.out.println ( Collections.frequency ( programmingLanguages, "Scala" ) );
